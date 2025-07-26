63d9c91b4622bfe37ffd1924a2212f1ba73fd46a
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-07-26 16:28:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,39 +1,26 @@
 // local mysql
 
-const mysql = require('mysql2/promise');
 
-const db = mysql.createPool({
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'buyerseller',
-    multipleStatements: true
-});
-
- 
-console.warn('Connected');
-
-module.exports = db;
 
 
 // live server
 
-// const mysql = require('mysql2/promise');
+const mysql = require('mysql2/promise');
 
-// const db = mysql.createPool({
-//   host: 'ballast.proxy.rlwy.net',
-//   port: 19598,
-//   user: 'root',
-//   password: 'wmnatvbnKaNnCTXDJZoyfrsErUboJPpk',
-//   database: 'railway', // Replace with actual DB name if different
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0
-// });
+const db = mysql.createPool({
+    host: 'turntable.proxy.rlwy.net',
+    port: 49229,
+    user: 'root',
+    password: 'uDgydrXReLDVobOSPJFiIrFEkExNShjv',
+    database: 'railway',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
-// console.warn('✅ Connected to Railway MySQL');
-// module.exports = db;
+console.warn('✅ Connected to Railway MySQL');
+
+module.exports = db;
 
 
 
