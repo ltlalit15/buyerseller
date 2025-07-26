e9e676a5b8444cbe64fe8a50e8b19d7adcc60e29
Sagarkher <sagarkher1999@gmail.com>
2025-07-26 16:21:47 +0530

    first commit

---
+++ routes/productRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct, getAllInventoryProducts } = require('../controller/product');
+const authMiddleware = require('../middleware/authMiddleware');
+const multer = require('multer');
+const path = require('path');
+
+const router = express.Router();
+
+router.post('/createProduct', authMiddleware, createProduct);
+router.get('/getAllProducts',  getAllProducts);
+router.get('/getProductById/:id', getProductById);
+router.patch('/updateProduct/:id', authMiddleware, updateProduct);
+router.delete('/deleteProduct/:id', authMiddleware, deleteProduct);
+
+router.get('/getAllInventoryProducts',  getAllInventoryProducts);
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
